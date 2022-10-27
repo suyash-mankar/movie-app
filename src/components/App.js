c26343cc70577786417677395ba38dcf3bb03424
suyash-mankar <suyashmankar@gmail.com>
2022-10-27 12:18:49 +0530

    create UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
+import {data} from '../data';
+import MovieCard from './MovieCard';
+import Navbar from './Navbar';
+
 function App() {
   return (
     <div className="App">
-      <h1> working </h1>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie)=> (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+
+      </div>
     </div>
   );
 }
