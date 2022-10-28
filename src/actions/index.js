374356f72d26a9285e1d0b08b7a22c6983d221a3
suyash-mankar <suyashmankar@gmail.com>
2022-10-28 16:52:00 +0530

    add favourite movie feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@
 
 // action types
 export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_FAVOURITE = "ADD_FAVOURITE";
 
 // action creators
 export function addMovies(movies) {
@@ -13,3 +14,10 @@ export function addMovies(movies) {
     movies,
   };
 }
+
+export function addFavourite(movie) {
+    return {
+      type: ADD_FAVOURITE,
+      movie,
+    };
+  }
--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,39 @@
-import {data} from '../data';
+import { data } from "../data";
 import React from "react";
 import MovieCard from "./MovieCard";
 import Navbar from "./Navbar";
-import { addMovies } from '../actions';
+import { addMovies } from "../actions";
 
 class App extends React.Component {
-
-
   componentDidMount() {
     // make api call
     // dispatch action
 
-    const {store} = this.props;
-    store.subscribe(()=> {
-      console.log('UPDATED');
+    const { store } = this.props;
+    store.subscribe(() => {
+      console.log("UPDATED");
       this.forceUpdate();
-    })
+      console.log("STATE", this.props.store.getState());
+    });
 
     store.dispatch(addMovies(data));
-    console.log('STATE', this.props.store.getState());
-
   }
 
+  isMovieFavourite = (movie) => {
+    const { favourites } = this.props.store.getState().movies;
+
+    const index = favourites.indexOf(movie);
+
+    if (index !== -1) {
+      // found the movie
+      return true;
+    }
+    return false;
+  };
+
   render() {
-    const {list} = this.props.store.getState().movies;
+    const { list } = this.props.store.getState().movies; // movies : {list: Array(12), favourites: Array(2)}
+
     return (
       <div className="App">
         <Navbar />
@@ -35,7 +45,12 @@ class App extends React.Component {
 
           <div className="list">
             {list.map((movie, index) => (
-              <MovieCard movie={movie} key={`movies-${index}`} />
+              <MovieCard
+                movie={movie}
+                dispatch={this.props.store.dispatch}
+                key={`movies-${index}`}
+                isFavourite={this.isMovieFavourite(movie)}
+              />
             ))}
           </div>
         </div>
